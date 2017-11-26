89cbd3cdae966f0abbc68da2c20c1b64a4a9628d
Dylan Sheffer <dsheffer16@gmail.com>
2017-11-26 14:06:01 -0500

    mobile navigation collapsing

--- js/dylan.js
+++ js/dylan.js
@@ -1,12 +1,17 @@
 // Hero Noun Changing
-document.onreadystatechange = () => {
-  const noun = document.querySelector("#noun");
-  let nouns = ["Developer", "Designer", "Bartender", "Tea&nbsp;Enthusiast","Dog&nbsp;Lover"];
-  noun.innerHTML = nouns[0];
-  let num = 0;
+const noun = document.querySelector("#noun");
+let nouns = ["Developer", "Designer", "Bartender", "Tea&nbsp;Enthusiast","Dog&nbsp;Lover"];
+noun.innerHTML = nouns[0];
+let num = 0;
 
-  window.setInterval(() => {
-    num >= nouns.length - 1 ? num = 0 : num++;
-    noun.innerHTML = nouns[num];
-  }, 1000)
-}
+window.setInterval(() => {
+  num >= nouns.length - 1 ? num = 0 : num++;
+  noun.innerHTML = nouns[num];
+}, 1000);
+
+
+// Mobile Navigation Collapsing
+document.querySelector('#show-menu').addEventListener('click', function (e){
+  const nav = document.querySelector('.nav');
+  nav.classList.toggle('nav--invisible');
+}, true);
